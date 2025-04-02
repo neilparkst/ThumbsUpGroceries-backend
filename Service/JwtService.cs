9e485806e14fbe7f38876897b5be5e628dc2fdc5
Neil Park <neil.park.st@gmail.com>
2025-04-03 00:09:23 +1300

    add JwtService to decode jwt token

---
+++ Service/JwtService.cs
@@ -0,0 +1,34 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace ThumbsUpGroceries_backend.Service
+{
+    public class JwtService
+    {
+        public static string GetClaimFromToken(string token, string claimType)
+        {
+            if (token.StartsWith("Bearer "))
+            {
+                token = token.Substring(7);
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var jwtToken = tokenHandler.ReadJwtToken(token);
+
+            // Return the claim value or null if not found
+            return jwtToken.Claims.FirstOrDefault(c => c.Type == claimType)?.Value;
+        }
+
+        public static Dictionary<string, string> GetAllClaimsFromToken(string token)
+        {
+            if (token.StartsWith("Bearer "))
+            {
+                token = token.Substring(7);
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var jwtToken = tokenHandler.ReadJwtToken(token);
+
+            return jwtToken.Claims.ToDictionary(c => c.Type, c => c.Value);
+        }
+    }
+}
