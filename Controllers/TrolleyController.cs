4d89c4ab1dea65b3deff6a24607c3b7afc787ee5
Neil Park <neil.park.st@gmail.com>
2025-04-03 23:17:02 +1300

    setup trolley skeleton

---
+++ Controllers/TrolleyController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThumbsUpGroceries_backend.Data;
+
+namespace ThumbsUpGroceries_backend.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class TrolleyController : ControllerBase
+    {
+        private readonly IDataRepository _dataRepository;
+
+        public TrolleyController(IDataRepository dataRepository)
+        {
+            _dataRepository = dataRepository;
+        }
+    }
+}
---
+++ Data/Models/Trolley.cs
@@ -0,0 +1,9 @@
+﻿namespace ThumbsUpGroceries_backend.Data.Models
+{
+    public class Trolley
+    {
+        public int TrolleyId { get; set; }
+        public Guid UserId { get; set; }
+        public int ItemCount { get; set; }
+    }
+}
