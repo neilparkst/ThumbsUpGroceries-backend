317d63df65318870a229b41b0a48960c093390b5
Neil Park <neil.park.st@gmail.com>
2025-04-11 00:37:44 +1200

    setup user model

---
+++ Data/Models/User.cs
@@ -0,0 +1,27 @@
+﻿namespace ThumbsUpGroceries_backend.Data.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Role { get; set; }
+    }
+
+    public class UserInfoResponse
+    {
+        public Guid UserId { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
